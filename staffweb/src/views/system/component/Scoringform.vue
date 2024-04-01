d662c31c7e91cca49e440327248e43d96118be28
shl240 <2401707410@qq.com>
2024-04-01 23:20:13 +0800

    根据4月1日会议提出的修改建议 ，对用户任务评分修改： 1 增加分数实时排名功能 2 对于重复分数不予写入 3 对用户进行提醒

--- staffweb/src/views/system/component/Scoringform.vue
+++ staffweb/src/views/system/component/Scoringform.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { defineProps ,ref} from 'vue';
 import type { FormInstance } from 'element-plus'
-
-
+import { ITEM_RENDER_EVT } from 'element-plus/es/components/virtual-list/src/defaults';
+import { isNull, isNumber, last } from 'lodash';
+import { ElMessageBox } from 'element-plus'
+import { trace } from 'console';
+const dialogVisible = ref(false)
 const formRef = ref<FormInstance | null>(null)
-
+const errmessage=ref({
+  lastnum:'',
+  nextnum:''
+})
 let mySet: Set<object> = new Set();
 
 interface evaItem{
@@ -12,8 +18,10 @@ interface evaItem{
   office:String,
   name:String,
   ID:String,          //ID
-  score:number
+  score:number,
+  rank:number,
 }
+
 
 const gridData :evaItem[]=[
   {
@@ -21,14 +29,18 @@ const gridData :evaItem[]=[
     office:'总经理',
     ID:'1',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
   {
     department:'长春分公司',
     office:'总经理',
     ID:'2',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+  
 
   },
   {
@@ -36,28 +48,36 @@ const gridData :evaItem[]=[
     office:'总经理',
     ID:'3',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
   {
     department:'长春分公司',
     office:'总经理',
     ID:'4',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
   {
     department:'长春分公司',
     office:'总经理',
     ID:'5',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
   {
     department:'长春分公司',
     office:'总经理',
     ID:'6',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
 
   },
   {
@@ -65,18 +85,24 @@ const gridData :evaItem[]=[
     office:'总经理',
     ID:'7',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
   {
     department:'长春分公司',
     office:'总经理',
     ID:'8',
     name: 'Peter Parker',
-    score:NaN
+    score:NaN,
+    rank:-1
+
   },
 ]
 
 function hasDuplicateScores(gridData:any) {
+
+//查重
     let i=0,j=0;
     for (i=0;i<gridData.length;i++) {
         for(j=i+1;j<gridData.length;j++){
@@ -90,14 +116,70 @@ function hasDuplicateScores(gridData:any) {
 
 function changestyle(rows:any){
   const [num1, num2] = hasDuplicateScores(gridData)
+
+  gridData.forEach(item => {
+    if(typeof item.score === 'object'||isNaN(item.score)){
+      item.rank = -1
+    }
+  });
+
+
+
+  const filteredData = gridData.filter(item => !(isNaN(item.score)||typeof item.score === 'object'));
+  filteredData.sort((a, b) => b.score - a.score);
+  filteredData.forEach((item, index) => {
+      item.rank = index + 1;
+  });
   if(num1===rows.rowIndex||num2===rows.rowIndex){
-    console.log([num1,num2])
+
     return { color:'#8B000F'}
   }else {
     return { color:'#00008B'}
   }
 }
+
+const handlesame =(row:any)=>{
+  console.log("**********************************")
   
+  const [num1, num2] = hasDuplicateScores(gridData)
+  if(!(num1===num2)){
+    
+    let flag:boolean=false;
+    let target:number=gridData[num1].score;
+    console.log(target)
+    row.score=NaN;
+    
+    const filteredData = gridData.filter((item, index) => {return !(index=== num1);}).filter(item => !(isNaN(item.score)||typeof item.score === 'object'));
+    filteredData.sort((a, b) => b.score-a.score);
+    console.log(filteredData)
+    let min:number=60;
+    let max:number=100;
+    
+    for (const item of filteredData) {
+        if (target > item.score) {
+          console.log(item.score);
+          target = item.score;
+          flag = true;
+          break; // 使用 break 跳出整个循环
+        }
+        max = item.score;
+    }
+
+    if(flag){
+      errmessage.value.lastnum=target.toString();
+      errmessage.value.nextnum=max.toString();
+    }else{
+      errmessage.value.lastnum=min.toString();
+      errmessage.value.nextnum=max.toString();
+    }
+    console.log("**********************************")
+    dialogVisible.value = true;
+  }
+}
+
+const handleClose = (done: () => void) => {
+
+}
 
 
 
@@ -120,6 +202,23 @@ const inputValue = ref<string>('');
 
 
 <template>
+
+<el-dialog
+    v-model="dialogVisible"
+    v-bind="errmessage"
+    title="Tips"
+    width="500"
+    :before-close="handleClose"
+  >
+    <span>输入重复，请重新输入 ,{{ errmessage.lastnum }} 和 {{ errmessage.nextnum }}</span>
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </div>
+    </template>
+  </el-dialog>
   <el-form
     ref="formRef"
     label-width="auto"
@@ -132,13 +231,13 @@ const inputValue = ref<string>('');
       <el-table-column property="name" label="姓名" width="200" />
       <el-table-column label="得分" width="250">
         <template v-slot:default="{ row }">
-            <el-input-number v-model="row.score" controls-position="right" :precision="2" :step="0.01" :min="60" :max="100"/>
+            <el-input-number v-model="row.score" controls-position="right" :precision="2" :step="0.01" :min="60" :max="100" @keyup.enter="handlesame(row)" @blur="handlesame(row)"/>
+
         </template>
       </el-table-column>
+    <el-table-column  label="排名" property="rank" />
     </el-table>
   </el-form> 
-
-  
 </template>
 
 
--- web/src/views/system/Taskmanage/presentTask/index.vue
+++ web/src/views/system/Taskmanage/presentTask/index.vue
@@ -1,3 +1,67 @@
-<template>
+<script setup lang="ts">
+import { ref, onMounted ,nextTick} from 'vue';
+
+
+const judge=ref(true);
+const revert=()=>{
     
+
+
+
+
+
+    judge.value=false;
+}
+interface evaluatedPerson{
+    name:String,
+    ID:String,
+}
+
+const griddata:evaluatedPerson[]=[{
+        name:'xiaoming',
+        ID:'1'
+},
+{
+        name:'xiaomhong',
+        ID:'2'
+}
+]
+
+
+
+
+</script>
+
+
+<template>
+    <div class="evaluated_container">
+        <el-empty v-if="judge" id="emptyElement" description="Empty" style="width: 100%;" @click="revert"/>
+        <el-table v-if="!judge" id="EvaluatedTable"
+                  :data="griddata">
+                  <el-table-column prop="name" label="名字"/>
+                  <el-table-column prop="ID" label="编号"/>
+        </el-table>
+    </div>
 </template>
+<style>
+.evaluated_container{
+    width: 100%; /* 让 div 宽度充满父容器 */
+    height: 100vh; /* 让 div 高度充满整个视窗 */
+    box-sizing: border-box; /* 让 padding 和 border 不会增加元素的宽度和高度 */
+    padding: 20px; /* 可选：为了美观，添加一些内边距 */
+}
+.el-empty{
+    /* border: 1px solid rgb(100, 100, 100); 添加一个 2px 宽度的黑色边框 */
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); /* 添加一个 10px 的模糊黑色阴影 */
+    /* background-color: lightgray; 当鼠标悬停时，改变背景色为浅灰色 */
+    
+}
+/* .el-table{
+    display: none;
+} */
+
+.el-empty:hover{
+    background-color: lightgray; /* 当鼠标悬停时，改变边框颜色为深灰色 */
+}
+
+</style>
