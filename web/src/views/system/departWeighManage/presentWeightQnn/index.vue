f137137fa4a22827bbe6b76e92dbb11af1a8360b
shl240 <2401707410@qq.com>
2024-04-25 17:03:17 +0800

    fix:拼写错误

--- web/src/views/system/departWeighManage/presentWeightQnn/index.vue
+++ web/src/views/system/departWeighManage/presentWeightQnn/index.vue
@@ -1,10 +1,15 @@
-
 <script setup lang="ts">
+
+
 
 </script>
 <template>
 
-
+<div style="padding: 30px;">
+    <div class="container mx-auto shadow-2xl ...">
+        111111111111
+    </div>
+</div>
 
 </template>
 <style scoped>
